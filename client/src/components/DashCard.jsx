53be320bc0486e8d343f978d7eb6d75276323361
nishav07 <nishavvv7@gmail.com>
2026-04-27 15:02:43 +0530

    idx filter update

--- client/src/components/DashCard.jsx
+++ client/src/components/DashCard.jsx
@@ -1,7 +1,10 @@
-export default function DashCard(){
+export default function DashCard({title="lol",body="test"}){
     return(<>
-    <div id="dashCards">
-        
+    <div id="dashCards" className="min-h-30 min-w-85 border-2 border-black flex items-center justify-center rounded-lg m-2">
+       <div className="flex flex-col gap-1">
+         <h1 className="text-2xl font-weight-500">{title}</h1>
+        <p>{body}</p>
+       </div>
     </div>
     </>)
 }
--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,21 @@
 import { useLeads } from "../../features/leadContext"
 import api from "../../helperFx/api";
 import { useToast } from "../../features/ToastContext";
+import DashCard from "../../components/DashCard";
+import { useEffect,useState } from "react";
+
 export default function Dashboard(){
+  let [filterData,setFilterData] = useState("");
+
+  useEffect(() => {
+
+  },[])
 
   return (<>
-  <div id="dashBoard">
-    
+  <div id="dashBoard" className="w-full flex flex-wrap ">
+    <DashCard/>
+     <DashCard/>
+      <DashCard/>
   </div>
   </>)
 }
